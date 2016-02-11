3f253f913d94b7793244a57444b8378960baad17
Ambermeetsrobot <amber@empiricalux.com>
2016-02-10 21:06:38 -0800

    add index.html formatting and content and include scripts.js queries and css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
-
+//show milk and cookies for under 21
   if (age < 21) {
     $('#milk').show();
-    var cookie = confirm("Would you like a cookie with your milk?");
-    if (cookie) {
-      $('#cookie'.show();
-    }
-  } else if (age >= 21) {
-    $('#wine').show();
-    var cheese = confirm("Would you like cheese with your wine?");
-    if (cheese) {
-      $('#cheese').show();
-    }
-  } else if (age > 50)
-    $('#prune-juice').show();
-    var fiber = confirm("Would you like some fiber with your prune juice?");
-    if (fiber) {
-      $('#fiber').show();
+
+    var cookieresponse = confirm("Would you like something to eat with your milk?");
+
+    if (cookieresponse) {
+      $('#cookie').show();
   }
+}
+//show wine and cheese for 21-49 y/os
+if ((age > 20) && (age < 50)) {
+  $('#wine').show();
+
+  var cheese = confirm("Would you like something to eat with your wine?");
+
+  if (cheese) {
+    $('#cheese').show();
+  }
+}
+//show prune-juice and fiber for 50+
+if (age >= 50) {
+  $('#prune-juice').show();
+
+  var fiber = confirm("Would you like something to eat with your prune juice?");
+
+  if (fiber) {
+    $('#fiber').show();
+  }
+}
+
 });
